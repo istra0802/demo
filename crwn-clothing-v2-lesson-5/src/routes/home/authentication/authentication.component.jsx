4c18e334aafadb93883e6994cb28451ef960d9cd
istra0802 <istra0802@gmail.com>
2024-02-01 19:12:28 +0530

    hi

---
+++ crwn-clothing-v2-lesson-5/src/routes/home/authentication/authentication.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {signInWithGooglePopup } from '../../../utils/firebase/firebase.utils'
+import { createUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils';
+import SignUpForm from '../../../components/sign-in-form/sign-up-form.component';
+import SignInForm from '../../../components/sign-in-form/sign-in-form.component';
+
+export default function Authentication() {
+
+  const logGoogleUser = async () =>
+  {
+    const {user} = await signInWithGooglePopup();
+    await  createUserDocumentFromAuth(user)
+  }
+
+  
+  return (
+    <div>
+
+      <h1>Sign in page </h1>
+      <SignInForm/>
+      <SignUpForm />
+    </div>
+  )
+}
---
+++ crwn-clothing-v2-lesson-5/src/routes/home/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo  } from "../../../assets/crown.svg"
+import "./navigation.styles.scss"
+export default function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+        
+        <CrwnLogo className="logo"></CrwnLogo>
+        </Link>
+        <div className="nav-links-container">
+            <Link className="nav-links" to = '/shop'>
+                SHOP
+            </Link>
+            <Link className="nav-links" to = '/sign-in'>
+                SIGN IN
+            </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
