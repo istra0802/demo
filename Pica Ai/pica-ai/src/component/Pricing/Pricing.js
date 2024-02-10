12b67b4f4e44364dcd3be4733ba29543eb90dee8
istra0802 <istra0802@gmail.com>
2024-02-10 13:30:10 +0530

    added pricing component

--- Pica Ai/pica-ai/src/component/Pricing/Pricing.js
+++ Pica Ai/pica-ai/src/component/Pricing/Pricing.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./Pricing.scss";
 
 export default function Pricing() {
   return (
-    <div>Pricing</div>
-  )
+    <div className="price-wrapper">
+      <h1
+        style={{
+          margin: "0px",
+          fontWeight: "700",
+          fontSize: "48px",
+          lineHeight: "60px",
+          textAlign: "center",
+          marginBottom: "60px",
+        }}
+      >
+        Pricing Plans
+      </h1>
+      <div className="price-list">
+        <div style={{ width: "auto" }}>
+          <div className="price-list-item">
+            <div className="price-item-title">Monthly Plan</div>
+            <div className="price-item-info">
+              <div className="price-item-amount">"$" "9.99"</div>
+              <div className="price-item-period">"/" " month"</div>
+            </div>
+            <div className="price-plan-info">"10000" "credits"</div>
+            <div className="price-plan-button">Subscribe Now</div>
+          </div>
+        </div>
+        <div style={{ width: "auto" }}>
+          <div className="price-list-item">
+            <div className="price-item-title">Annual Plan</div>
+            <div className="price-item-info">
+              <div className="price-item-amount">"$" "3.33"</div>
+              <div className="price-item-period">"/" " month"</div>
+            </div>
+            <div className="price-plan-info">"US$39.99 billed yearly"</div>
+            <div className="price-plan-info">"10000" "credits"</div>
+            <div className="price-plan-button price-plan-button-active">Subscribe Now</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
